283e77dfad192271197a2f259ec975b61e9277a1
CR-Aylin <147929737+CR-Aylin@users.noreply.github.com>
2024-06-26 18:22:14 -0400

    mov

---
+++ Assets/scripts/playermov.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermov : MonoBehaviour
+{
+    // declaro variable para ajustar velocidad del jugador
+    [SerializeField]private float speed = 3f;
+    // referencia al rigibody
+    private Rigidbody2D Playerpipipi;
+    private Vector2 moveInput;
+
+
+    void Start()
+    {
+        //obtener ref
+        Playerpipipi = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movX = Input.GetAxisRaw("Horizontal"); //valor -1 o 1
+        float movY = Input.GetAxisRaw("Vertical"); //valor -1 o 1
+        moveInput = new Vector2(movX,movY).normalized;
+    }
+
+    private void FixedUpdate(){// Fisicas
+    // mover atraves de rigidbody
+    //posicion actual + mov dado por entradas y controlar velocidad 
+
+    Playerpipipi.MovePosition(Playerpipipi.position + moveInput * speed * Time.fixedDeltaTime);
+
+
+    }
+
+}
