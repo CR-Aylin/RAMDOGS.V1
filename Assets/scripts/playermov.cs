70a346c9c992684a2ff7cdc425119c4e313cd49e
CR-Aylin <147929737+CR-Aylin@users.noreply.github.com>
2024-06-27 00:34:08 -0400

    animacion pipipi

--- Assets/scripts/playermov.cs
+++ Assets/scripts/playermov.cs
@@ -10,11 +10,14 @@ public class playermov : MonoBehaviour
     private Rigidbody2D Playerpipipi;
     private Vector2 moveInput;
 
+    private Animator animacionplayer;
+
 
     void Start()
     {
-        //obtener ref
+        //obtener ref//citar
         Playerpipipi = GetComponent<Rigidbody2D>();
+        animacionplayer = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
@@ -23,6 +26,10 @@ public class playermov : MonoBehaviour
         float movX = Input.GetAxisRaw("Horizontal"); //valor -1 o 1
         float movY = Input.GetAxisRaw("Vertical"); //valor -1 o 1
         moveInput = new Vector2(movX,movY).normalized;
+
+        animacionplayer.SetFloat("Horizontal",movX);
+        animacionplayer.SetFloat("Vertical",movY);
+        animacionplayer.SetFloat("speed",moveInput.sqrMagnitude);
     }
 
     private void FixedUpdate(){// Fisicas
@@ -30,6 +37,7 @@ public class playermov : MonoBehaviour
     //posicion actual + mov dado por entradas y controlar velocidad 
 
     Playerpipipi.MovePosition(Playerpipipi.position + moveInput * speed * Time.fixedDeltaTime);
+    
 
 
     }
