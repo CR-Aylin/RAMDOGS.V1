19f2d44cbba62f815941ba81c43f120f46a63d1a
CR-Aylin <147929737+CR-Aylin@users.noreply.github.com>
2024-06-30 02:01:09 -0400

    moneda
    no la cuenta ni desaparece

---
+++ Assets/scripts/Moneda.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Moneda : MonoBehaviour
+{
+   public delegate void SumaMoneda(int moneda);
+   public static event SumaMoneda sumaMoneda;
+   [SerializeField] private int cantidades;
+
+   private void OnTriggerEnter2D(Collider2D collision){
+
+    if(collision.gameObject.CompareTag("Player")){
+
+        if(sumaMoneda != null){
+            SumarMoneda();
+            Destroy(this.gameObject);
+        }
+    }
+
+
+
+   }
+
+   private void SumarMoneda(){
+
+    sumaMoneda(cantidades);
+
+   }
+}
